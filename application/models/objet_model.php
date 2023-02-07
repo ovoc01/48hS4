4651f3e79897f707b85eb3aa8d53fa429be7504c
TheKraken9 <andrianambininafenitra@gmail.com>
2023-02-07 15:32:31 +0300

    for database schemas and the object model

--- application/models/objet_model.php
+++ application/models/objet_model.php
@@ -2,7 +2,33 @@
 
 class objet_model extends CI_Model
 {
-    function getObjet($idcategorie) {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
+    public function getObjet($idcategorie) {
+        $query = $this->db->get_where('objet',array('idcategorie' => $idcategorie));
+        return $query->result_array();
+    }
+
+    public function getAllObjet() {
+        $query = $this->db->get('objet');
+        return $query->result_array();
+    }
+
+    public function getUtilisateurObjets($idutilisateur) {
+        $query = $this->db->get_where('objet',array('idutilisateur' => $idutilisateur));
+        return $query->result_array();
+    }
+
+    public function save($idUtilisateur, $prixEstime, $titre, $description, $idCategorie)
+    {
+        $this->db->insert('objet', array('idutilisateur' => $idUtilisateur, 'idcategorie' => $idCategorie, 'prixestimatif' => $prixEstime, 'titre' => $titre, 'description' => $description));
+    }
+
+    public function getLastId() {
+        $result = $this->db->query("select max(idobjet) from objet")->result();
+        return $result;
     }
 }
