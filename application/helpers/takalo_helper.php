5de53f3661d9b1ba273afceafd70ea9e7e29bce3
TheKraken9 <andrianambininafenitra@gmail.com>
2023-02-08 08:48:19 +0300

    commit for one helper

--- application/helpers/takalo_helper.php
+++ application/helpers/takalo_helper.php
@@ -7,4 +7,20 @@ if( ! function_exists('checkLogIn')){
         if(count($result)==1) return $result[0];
         return false;
     }
+
+    function upload() {
+        $file_count = count($_FILES['file']['name']);
+        $img_string = "";
+        for ($i = 0; $i < $file_count; $i++) {
+            $filename = $_FILES['file']['name'][$i];
+            if (in_array(strchr($filename, "."), array('.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'))) {
+                move_uploaded_file($_FILES['file']['tmp_name'][$i], 'assets/images/'.$filename);
+                $img_string .= $filename;
+
+                if ($i < $file_count - 1) {
+                    $img_string .= ",";
+                }
+            }
+        }
+    }
 }
