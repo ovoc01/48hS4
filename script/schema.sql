eb5d657d1dd3f72e8c75226b888ab53ebbafd5c9
Pakito033 <119033755+Pakito033@users.noreply.github.com>
2023-07-09 16:39:50 -0400

    user, user_view

---
+++ script/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user (
+    idUtilisateur int PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(30) DEFAULT NULL,
+    prenom VARCHAR(30) DEFAULT NULL,
+    email VARCHAR(50) NOT NULL,
+    mdp VARCHAR(50) NOT NULL,
+    status INTEGER DEFAULT 10
+);
