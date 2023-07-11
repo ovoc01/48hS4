98436c808811c520a4758583ee5f65da7be814fd
Pakito033 <119033755+Pakito033@users.noreply.github.com>
2023-07-11 07:24:06 -0100

    config

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost:1234/48hS4/';
+$config['base_url'] = 'http://localhost/48hS4/';
 
 /*
 |--------------------------------------------------------------------------
