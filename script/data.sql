0df999b35c85e8301ed4f3d4dc08e6fdf4190e3d
ovoc01 <spotifymirindra@gmail.com>
2023-07-11 08:14:18 +0300

    Remove all main files and folders to resolve conflict

--- script/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO user (nom, prenom, email, mdp, status) VALUES
-    ('Test', 'Toto', 'test@gmail.com', '1234', DEFAULT);
-
-INSERT INTO genre (intitule) VALUES
-    ('Homme'),
-    ('Femme'),
-    ('Ne pas préciser');
